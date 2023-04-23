759159d3cf0adb6533e79b7e302e5d61a007cc63
Takudzwamz <zhout1709@hotmail.com>
2023-04-23 17:31:36 +0300

    Happy coding, added new fast workers

--- final_crawler.py
+++ final_crawler.py
@@ -1,3 +1,90 @@
+# import requests
+# import threading
+# import queue
+# from bs4 import BeautifulSoup
+# from urllib.parse import urlparse, urljoin
+
+
+# def crawl(max_pages=50):
+#     internal_links = set()
+#     subdomain_links = set()
+#     external_links = set()
+#     document_links = set()
+#     broken_links = set()
+#     visited_links = set()
+
+#     def worker():
+#         while True:
+#             url = q.get()
+#             if url in visited_links or len(visited_links) >= max_pages:
+#                 q.task_done()
+#                 continue
+#             visited_links.add(url)
+#             try:
+#                 response = requests.get(url)
+#             except:
+#                 broken_links.add(url)
+#                 q.task_done()
+#                 continue
+#             soup = BeautifulSoup(response.text, "lxml")
+#             for a_tag in soup.find_all("a", href=True):
+#                 href = a_tag.get("href")
+#                 if href and not href.startswith("#"):
+#                     joined_link = urljoin(url, href)
+#                     parsed_link = urlparse(joined_link)
+#                     if parsed_link.scheme not in ["http", "https"]:
+#                         continue
+#                     if parsed_link.fragment:
+#                         joined_link = joined_link.split("#")[0]
+#                         parsed_link = urlparse(joined_link)
+#                     if is_internal(parsed_link):
+#                         if is_subdomain(parsed_link):
+#                             subdomain_links.add(joined_link)
+#                         else:
+#                             internal_links.add(joined_link)
+#                         if len(visited_links) < max_pages:
+#                             q.put(joined_link)
+#                     elif is_external(parsed_link):
+#                         external_links.add(joined_link)
+#                     else:
+#                         continue
+#                 elif href and href.endswith((".doc", ".docx", ".pdf")):
+#                     document_links.add(urljoin(url, href))
+#             q.task_done()
+
+#     def is_internal(parsed_link):
+#         return not parsed_link.netloc or parsed_link.netloc == root_netloc
+
+#     def is_subdomain(parsed_link):
+#         return parsed_link.hostname.endswith(root_netloc)
+
+#     def is_external(parsed_link):
+#         return parsed_link.netloc and not is_subdomain(parsed_link)
+
+#     url = input("Enter a URL to crawl:")
+#     parsed_url = urlparse(url)
+#     root_netloc = parsed_url.hostname
+
+#     q = queue.Queue()
+#     q.put(url)
+
+#     for i in range(20):
+#         t = threading.Thread(target=worker)
+#         t.daemon = True
+#         t.start()
+
+#     q.join()
+
+#     print(f"Internal Links ({len(internal_links)}):\n{internal_links}")
+#     print(f"Subdomain Links ({len(subdomain_links)}):\n{subdomain_links}")
+#     print(f"External Links ({len(external_links)}):\n{external_links}")
+#     print(f"Document Links ({len(document_links)}):\n{document_links}")
+#     print(f"Broken Links ({len(broken_links)}):\n{broken_links}")
+
+
+# crawl()
+
+
 from urllib.parse import urljoin, urlparse
 import requests
 from bs4 import BeautifulSoup
@@ -23,9 +110,9 @@ ORANGE = colorama.Fore.LIGHTRED_EX
 MEROON = colorama.Fore.LIGHTMAGENTA_EX
 CYAN = colorama.Fore.CYAN
 
-seed_url = "https://spbu.ru"  # ... your seed URL here
-max_visits = 1000  # ... max number of pages to visit
-num_workers = 20  # ... number of workers to run in parallel
+seed_url = "https://msu.ru"  # ... your seed URL here
+max_visits = 10000  # ... max number of pages to visit
+num_workers = 5  # ... number of workers to run in parallel
 
 visited = set()
 internal_urls = set()
@@ -38,7 +125,6 @@ document_urls = set()
 def is_valid(url):
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
-
 
 
 def is_skip_link(link):
@@ -68,13 +154,21 @@ def get_links(url):
     external_links = set()
     document_links = set()
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
+    try:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
+
+    except (requests.exceptions.RequestException, UnicodeError):
+        print(f"{RED}[*] Broken link: {url}{RESET}")
+        broken_urls.add(url)
+        return internal_links, subdomain_links, external_links, document_links, broken_urls
 
     # get internal and subdomain links
     for a_tag in soup.find_all("a", href=True):
         href = a_tag.get("href")
         if href.startswith("mailto:") or href.startswith("tel:") or href.startswith("#"):
+            continue
+        if is_skip_link(href):
             continue
         elif href.startswith("#"):
             internal_links.add(href)
@@ -92,7 +186,13 @@ def get_links(url):
     # get external links
     for a_tag in soup.find_all("a", href=True):
         href = a_tag.get("href")
+        if is_skip_link(href):
+            continue
         if href.startswith("mailto:") or href.startswith("tel:") or href.startswith("#"):
+            continue
+        elif href.endswith(".doc") or href.endswith(".docx") or href.endswith(".pdf") or href.endswith(".xls") or href.endswith(".xlsx"):
+            print(f"{PURPLE}[*] Document link: {href}{RESET}")
+            document_links.add(href)
             continue
         else:
             joined_link = urljoin(url, href)
@@ -101,15 +201,18 @@ def get_links(url):
                 print(f"{GRAY}[*] External link: {joined_link}{RESET}")
                 external_links.add(joined_link)
 
-    # get document links
-    for a_tag in soup.find_all("a", href=True):
-        href = a_tag.get("href")
-        if href.endswith(".doc") or href.endswith(".docx") or href.endswith(".pdf") or href.endswith(".xls") or href.endswith(".xlsx"):
-            print(f"{PURPLE}[*] Document link: {href}{RESET}")
-            document_links.add(href)
-            
+    # # get document links
+    # for a_tag in soup.find_all("a", href=True):
+    #     href = a_tag.get("href")
+    #     if is_skip_link(href):
+    #         continue
+    #     if href.endswith(".doc") or href.endswith(".docx") or href.endswith(".pdf") or href.endswith(".xls") or href.endswith(".xlsx"):
+    #         print(f"{PURPLE}[*] Document link: {href}{RESET}")
+    #         document_links.add(href)
+    #         continue
 
     return internal_links, subdomain_links, external_links, document_links
+
 
 def get_broken_links(url):
     internal_links, subdomain_links, external_links, document_links = get_links(
@@ -130,18 +233,52 @@ def get_broken_links(url):
 
 
 def extract_content(url):
-    internal_links, subdomain_links, external_links, document_links = get_links(
-        url)
+    result = get_links(url)
+
+    if len(result) < 4:
+        # Handle the case where get_links() returns less than four values
+        internal_links, subdomain_links, external_links, document_links = set(), set(), set(), set()
+    else:
+        internal_links, subdomain_links, external_links, document_links = result
+
+    for link in internal_links | subdomain_links:
+        if link not in visited:
+            q.put(link)
+
     internal_urls.update(internal_links)
     subdomains.update(subdomain_links)
     external_urls.update(external_links)
     document_urls.update(document_links)
 
-    for link in internal_links:
-        if link not in visited:
-            q.put(link)
+
+# def extract_content(url):
+#     internal_links, subdomain_links, document_links, external_links = get_links(
+#         url)
+
+#     for link in internal_links | subdomain_links:
+#         if link not in visited:
+#             q.put(link)
+
+#     internal_urls.update(internal_links)
+#     subdomains.update(subdomain_links)
+#     external_urls.update(external_links)
+#     document_urls.update(document_links)
+
+# def extract_content(url):
+#     internal_links, subdomain_links, external_links, document_links = get_links(
+#         url)
+#     internal_urls.update(internal_links)
+#     subdomains.update(subdomain_links)
+#     external_urls.update(external_links)
+#     document_links.update(document_links)
+
+#     for link in internal_links | subdomain_links:
+#         if link not in visited:
+#             q.put(link)
 
 # crawl the internal links and subdomain links
+
+
 def crawl(url):
     visited.add(url)
     # print("Crawl: ", url)
@@ -151,7 +288,9 @@ def crawl(url):
         if link not in visited:
             q.put(link)
 
-#worker thread
+# worker thread
+
+
 def queue_worker(i, q):
     while True:
         url = q.get()
@@ -167,6 +306,7 @@ for i in range(num_workers):
 q.put(seed_url)
 q.join()
 
+
 # print the results
 print("[+] Total visited links:", len(visited))
 print("[+] Total Internal links:", len(internal_urls))
@@ -174,7 +314,8 @@ print("[+] Total External links:", len(external_urls))
 print("[+] Total Subdomains links:", len(subdomains))
 print("[+] Total Broken links:", len(broken_urls))
 print("[+] Total Document links:", len(document_urls))
-print("[+] Total links:", len(internal_urls) + len(external_urls) + len(subdomains) + len(broken_urls) + len(document_urls))
+print("[+] Total links:", len(internal_urls) + len(external_urls) +
+      len(subdomains) + len(broken_urls) + len(document_urls))
 
 
 # create folder for the website
@@ -202,4 +343,3 @@ with open(f"{folder}/broken_urls.txt", "w", encoding='utf-8') as f:
 
 with open(f"{folder}/document_urls.txt", "w", encoding='utf-8') as f:
     f.write("\n".join(document_urls))
-
---
+++ spbu_worker.py
@@ -0,0 +1,181 @@
+import os
+from bs4 import BeautifulSoup
+import colorama
+import requests
+import requests.exceptions
+from urllib.parse import urlsplit
+from urllib.parse import urlparse
+from collections import deque
+import asyncio
+
+
+# init the colorama module
+colorama.init()
+
+GREEN = colorama.Fore.GREEN
+GRAY = colorama.Fore.LIGHTBLACK_EX
+RESET = colorama.Fore.RESET
+YELLOW = colorama.Fore.YELLOW
+PURPLE = colorama.Fore.MAGENTA
+RED = colorama.Fore.RED
+BLUE = colorama.Fore.BLUE
+CREAM = colorama.Fore.LIGHTYELLOW_EX
+ORANGE = colorama.Fore.LIGHTRED_EX
+MEROON = colorama.Fore.LIGHTMAGENTA_EX
+
+url = "https://spbu.ru"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+
+async def process_url(url, processed_urls, new_urls, total_urls_visited, broken_urls, document_urls, local_urls, subdomain_urls, foreign_urls):
+    processed_urls.add(url)
+    total_urls_visited += 1
+    print(f"{YELLOW}[*] Crawling: {url}{RESET}")
+    try:
+        response = await asyncio.get_event_loop().run_in_executor(None, requests.get, url)
+    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema):
+        print(f"{RED}[*] Broken link: {url}{RESET}")
+        broken_urls.add(url)
+        return
+
+    parts = urlsplit(url)
+    base = "{0.netloc}".format(parts)
+    strip_base = '.'.join(base.split('.')[-2:])
+    base_url = "{0.scheme}://{0.netloc}".format(parts)
+    path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+    
+
+    # soup = BeautifulSoup(response.content , "lxml")
+    # create a beutiful soup for the html document
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, "lxml")
+    # print(soup.prettify())
+    
+    for link in soup.find_all('a'):
+        anchor = link.attrs["href"] if "href" in link.attrs else ''
+
+        if anchor.startswith('mailto:') or anchor.startswith('tel:') or anchor.endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.mp4', '.avi', '.mov')):
+            continue
+        elif anchor.endswith(('.doc', '.docx', '.pdf', '.pptx', '.ppt', '.xls', '.xlsx', '.csv', '.txt', '.rtf', '.zip', '.rar')):
+            print(f"{MEROON}[*] Document link: {anchor}{RESET}")
+            document_urls.add(anchor)
+            continue
+        elif anchor.startswith('/'):
+            local_link = base_url + anchor
+            print(f"{GREEN}[*] Local link: {local_link}{RESET}")
+            local_urls.add(local_link)
+        elif strip_base in anchor or base in anchor:
+            parsed = urlparse(anchor)
+            if parsed.netloc == base or parsed.netloc == strip_base:
+                local_link = parsed.scheme + '://' + parsed.netloc + parsed.path
+                if parsed.query:
+                    local_link += '?' + parsed.query
+                if parsed.fragment:
+                    local_link += '#' + parsed.fragment
+                print(f"{GREEN}[*] Local link: {local_link}{RESET}")
+                local_urls.add(local_link)
+            else:
+                subdomains = parsed.netloc.split('.')
+                if len(subdomains) > 2 and subdomains[-2] == strip_base.split('.')[0] or (len(subdomains) > 3 and subdomains[-3] == strip_base.split('.')[0]):
+                    subdomain_urls.add(anchor)
+                    print(f"{BLUE}[*] Subdomain link: {anchor}{RESET}")
+                else:
+                    print(f"{GRAY}[*] Foreign link: {anchor}{RESET}")
+                    foreign_urls.add(anchor)
+        else:
+            print(f"{GRAY}[*] Foreign link: {anchor}{RESET}")
+            foreign_urls.add(anchor)
+
+    for i in local_urls.union(subdomain_urls):
+        if i not in new_urls and i not in processed_urls:
+            print(f"{PURPLE}[*] Adding to queue: {i}{RESET}")
+            new_urls.append(i)
+
+
+
+async def process_urls(new_urls, max_visits):
+    processed_urls = set()
+    total_urls_visited = 0
+    broken_urls = set()
+    document_urls = set()
+    local_urls = set()
+    subdomain_urls = set()
+    foreign_urls = set()
+
+    while new_urls and total_urls_visited < max_visits:
+        url = new_urls.popleft()
+        await process_url(url, processed_urls, new_urls, total_urls_visited, broken_urls, document_urls, local_urls, subdomain_urls, foreign_urls)
+        total_urls_visited += 1
+        if total_urls_visited >= max_visits:
+            break
+
+    return (processed_urls, broken_urls, document_urls, local_urls, subdomain_urls, foreign_urls)
+
+
+
+
+if __name__ == "__main__":
+    new_urls = deque([url])
+    max_visits = 1000  # ... max number of pages to visit
+    loop = asyncio.get_event_loop()
+    try:
+        processed_urls,broken_urls, document_urls, local_urls, subdomain_urls, foreign_urls = loop.run_until_complete(
+            process_urls(new_urls, max_visits))
+    finally:
+        loop.close()
+        
+    # Total URLs found
+    total_urls_visited = len(broken_urls) + len(document_urls) + len(local_urls) + len(subdomain_urls) + len(foreign_urls)   
+
+    # create a report of all urls
+    print("[+] Total Internal links:", len(local_urls))
+    print("[+] Total Subdomain links:", len(subdomain_urls))
+    print("[+] Total Foreign links:", len(foreign_urls))
+    print("[+] Total Document links:", len(document_urls))
+    print("[+] Total Broken links:", len(broken_urls))
+    print("[+] Total URLs found:", total_urls_visited)
+    print("[+] Total Processed URLs:", len(processed_urls))
+    print(f"Number of unprocessed links: {len(new_urls)}")
+
+    # create a report of all urls
+    # create folder for the website
+domain = urlparse(url).netloc
+folder = f"./{domain}"
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    
+# save all urls to a file
+with open(f"{folder}/total_urls_found.txt", "w", encoding='utf-8') as f:
+    f.write(str(total_urls_visited))
+
+# save all broken urls to a file
+with open(f"{folder}/broken_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(broken_urls))
+    
+# save all document urls to a file
+with open(f"{folder}/document_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(document_urls))
+
+# save all local urls to a file
+with open(f"{folder}/local_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(local_urls))
+    
+# save all subdomain urls to a file
+with open(f"{folder}/subdomain_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(subdomain_urls))
+    
+# save all foreign urls to a file
+with open(f"{folder}/foreign_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(foreign_urls))
+    
+# save all processed urls to a file
+with open(f"{folder}/processed_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(processed_urls))
+    
+# save all unprocessed urls to a file
+with open(f"{folder}/unprocessed_urls.txt", "w", encoding='utf-8') as f:
+    f.write("\n".join(new_urls))
+
